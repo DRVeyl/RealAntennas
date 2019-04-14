d9795dc49ac626d6af53595dd50fc5375e3be24f
DRVeyl <DRVeyl@gmail.com>
2019-04-14 18:37:24 -0400

    Kerbalism API Update Now passing MethodInfo instead of Delegate Set values for when not connected: do not allow processing to fall back to stock.

--- src/RealAntennasProject/Kerbalism/Kerbalism.cs
+++ src/RealAntennasProject/Kerbalism/Kerbalism.cs
@@ -7,60 +7,47 @@ using System.Reflection;
 
 namespace RealAntennas.Kerbalism
 {
-    public static class GenericDelegateFactory
-    {
-        public static object CreateDelegateByParameter(Type p1Type, Type p2Type, object target, MethodInfo method)
-        {
-            var createDelegate = typeof(GenericDelegateFactory).GetMethod("CreateDelegate").MakeGenericMethod(new Type[] { p1Type, p2Type });
-            return createDelegate.Invoke(null, new object[] { target, method });
-        }
-
-        public static Action<T1,T2> CreateDelegate<T1,T2>(object target, MethodInfo method)
-        {
-            return (Action<T1,T2>)Delegate.CreateDelegate(typeof(Action<T1,T2>), target, method);
-        }
-    }
     public class Kerbalism
     {
         public static readonly string ModTag = "[RAKerbalismLink] ";
         public static Assembly KerbalismAssembly = null;
-        public static void MyCommHandler<T>(T p1, Vessel v)
+        public static void MyCommHandler(object p1, Vessel v)
         {
-            if (p1.GetType() != KerbalismAssembly.GetType("KERBALISM.AntennaInfo"))
-            {
-                Debug.LogFormat(ModTag + "Somehow we got called with the wrong type for {0} = {1}", p1, p1.GetType());
-            }
             if (v.Connection is RACommNetVessel raCNV &&
                 raCNV.Comm is RACommNode node &&
-                node.AntennaTowardsHome() is RealAntenna ra &&
                 KerbalismAssembly.GetType("KERBALISM.AntennaInfo") is Type KerbalismAntennaInfoType)
             {
-                CommNet.CommPath path = new CommNet.CommPath();
-                (node.Net as RACommNetwork).FindHome(node, path);
-                double rate = (node.Net as RACommNetwork).MaxDataRateToHome(node) / 8e6;    // Convert rate from bps to MBps
-                double packetInterval = 1.0F;
-                double ec = ra.PowerDrawLinear * packetInterval * 1e-6;    // 1 EC/sec = 1KW.  Draw(mw) * interval(sec) * mW -> kW conversion
-                double strength = 0;
-                if (node[path.First.end] is RACommLink link)
+                double rate = 0, ec = 0, strength = 0, packetInterval = 1.0f;
+                int status = 2;
+                string target_name = "NotConnected";
+                List<string[]> sList = new List<string[]>();
+                if (node.AntennaTowardsHome() is RealAntenna ra)
                 {
-                    strength = link.start.Equals(node) ? link.FwdCI: link.RevCI;
+                    CommNet.CommPath path = new CommNet.CommPath();
+                    (node.Net as RACommNetwork).FindHome(node, path);
+                    status = !raCNV.IsConnectedHome ? 2 : path.Count == 1 ? 0 : 1;
+                    rate = (node.Net as RACommNetwork).MaxDataRateToHome(node) / 8e6;    // Convert rate from bps to MBps
+                    ec = ra.PowerDrawLinear * packetInterval * 1e-6;    // 1 EC/sec = 1KW.  Draw(mw) * interval(sec) * mW -> kW conversion
+                    if (node[path.First.end] is RACommLink link)
+                    {
+                        strength = link.start.Equals(node) ? link.FwdCI : link.RevCI;
+                    }
+                    foreach (CommNet.CommLink clink in path)
+                    {
+                        sList.Add(new string[1] { clink.end.name });
+                    }
+                    target_name = path.First.end.ToString();
                 }
 
                 p1.GetType().GetField("linked").SetValue(p1, raCNV.IsConnectedHome); // Link Status
-                p1.GetType().GetField("ec").SetValue(p1, ec);       // EC/s
-                p1.GetType().GetField("rate").SetValue(p1, rate);   // Rate in MB/s
-                p1.GetType().GetField("status").SetValue(p1, !raCNV.IsConnectedHome ? 2 : path.Count == 1 ? 0 : 1);      // 0=direct, 1=indirect, 2=none
+                p1.GetType().GetField("ec").SetValue(p1, ec);               // EC/s
+                p1.GetType().GetField("rate").SetValue(p1, rate);           // Rate in MB/s
+                p1.GetType().GetField("status").SetValue(p1, status);       // 0=direct, 1=indirect, 2=none
                 p1.GetType().GetField("strength").SetValue(p1, strength);   // Signal quality indicator (float 0..1)
-                p1.GetType().GetField("target_name").SetValue(p1, path.First.end.ToString());
-                List<string[]> sList = new List<string[]>();
-                foreach (CommNet.CommLink clink in path)
-                {
-                    sList.Add(new string[1] { clink.end.name });
-                }
+                p1.GetType().GetField("target_name").SetValue(p1, target_name);
                 p1.GetType().GetField("control_path").SetValue(p1, sList);
 
-                Debug.LogFormat(ModTag + "Rate: {0:F1} EC: {1:F4}  Linked:{2}  Strength: {3:F2}  Target: {4}  CPath: {5}",
-                    rate, ec, raCNV.IsConnectedHome, strength, path.First.end, sList);
+                Debug.LogFormat(ModTag + "Rate: {0:F1} EC: {1:F4}  Linked:{2}  Strength: {3:F2}  Target: {4}  CPath: {5}", rate, ec, raCNV.IsConnectedHome, strength, target_name, sList);
             }
 
             /*
@@ -73,15 +60,15 @@ namespace RealAntennas.Kerbalism
             {
                 if (a.name.StartsWith("Kerbalism") && 
                     !a.name.StartsWith("KerbalismBoot") &&
-                    a.assembly.GetType("KERBALISM.API") is Type KerbalismAPIType &&
-                    a.assembly.GetType("KERBALISM.AntennaInfo") is Type KerbalismAntennaInfoType &&
-                    KerbalismAPIType.GetField("Comm", BindingFlags.Public | BindingFlags.Static) is var comm 
+                    a.assembly.GetType("KERBALISM.API") is Type KerbalismAPIType
+//                    a.assembly.GetType("KERBALISM.AntennaInfo") is Type KerbalismAntennaInfoType &&
                     )
                 {
                     KerbalismAssembly = a.assembly;
                     MethodInfo baseMethod = typeof(Kerbalism).GetMethod("MyCommHandler");
-                    MethodInfo myGenericMethod = baseMethod.MakeGenericMethod(new Type[] { KerbalismAntennaInfoType });
-                    var x = GenericDelegateFactory.CreateDelegateByParameter(KerbalismAntennaInfoType, typeof(Vessel), null, myGenericMethod);
+//                    MethodInfo myGenericMethod = baseMethod.MakeGenericMethod(new Type[] { KerbalismAntennaInfoType });
+//                    var x = GenericDelegateFactory.CreateDelegateByParameter(KerbalismAntennaInfoType, typeof(Vessel), null, myGenericMethod);
+                    var x = baseMethod;
                     var fInf = KerbalismAPIType.GetField("Comm", BindingFlags.Public | BindingFlags.Static);
                     var val = fInf.GetValue(null);
                     var mInf = val.GetType().GetMethod("Add", BindingFlags.Instance | BindingFlags.Public);
