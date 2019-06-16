c9e9fa8099d709d2eb3f97d2ce7afc3972a1103f
DRVeyl <DRVeyl@gmail.com>
2019-06-16 12:07:17 -0400

    GUI Code Maintenance

--- src/RealAntennasProject/Antenna/AntennaGUI.cs
+++ src/RealAntennasProject/Antenna/AntennaGUI.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace RealAntennas.Antenna
@@ -11,45 +8,14 @@ namespace RealAntennas.Antenna
     // Many thanks for the demonstration for manipulating BaseField/BaseEvent and basic GUI use.
     public class AntennaGUI
     {
-        public Dictionary<BaseEvent, bool> enabledEvents = new Dictionary<BaseEvent, bool>();
-        public Dictionary<BaseField, bool> enabledFields = new Dictionary<BaseField, bool>();
         public bool showGUI = false;
         Rect Window = new Rect(20, 100, 240, 50);
-        Vector2 scrollPosition1, scrollPosition2, scrollVesselPos, scrollBodyPos;
-        bool showFields = false, showEvents = false;
+        Vector2 scrollVesselPos, scrollBodyPos;
         bool showVessels = false, showBodies = false;
-        List<BaseField> sortedFields;
-        List<BaseEvent> sortedEvents;
         public Part ParentPart { get; set; }
         public ModuleRealAntenna ParentPartModule { get; set; }
 
-        public void Start()
-        {
-            List<BaseField> myFieldList = new List<BaseField>();
-            List<BaseEvent> myEventList = new List<BaseEvent>();
-            foreach (BaseEvent e in ParentPart.Events)
-            {
-                if (e.guiActive) myEventList.Add(e);
-            }
-            foreach (BaseField bf in ParentPart.Fields)
-            {
-                if (bf.guiActive) myFieldList.Add(bf);
-            }
-            foreach (PartModule pm in ParentPart.Modules)
-            {
-                foreach (BaseField bf in pm.Fields)
-                {
-                    if (bf.guiActive) myFieldList.Add(bf);
-                }
-                foreach (BaseEvent e in pm.Events)
-                {
-                    int id = e.id;
-                    if (e.guiActive) myEventList.Add(pm.Events[id]);
-                }
-            }
-            sortedFields = myFieldList.OrderBy(baseField => baseField.guiName).ToList();
-            sortedEvents = myEventList.OrderBy(baseEvent => baseEvent.guiName).ToList();
-        }
+        public void Start() {}
 
         public void OnGUI()
         {
@@ -61,39 +27,6 @@ namespace RealAntennas.Antenna
 
         void GUIDisplay(int windowID)
         {
-            string label;
-            if (GUILayout.Button("Show Fields")) showFields = !showFields;
-            if (showFields)
-            {
-                scrollPosition1 = GUILayout.BeginScrollView(scrollPosition1, GUILayout.Width(200), GUILayout.Height(200));
-                foreach (BaseField bf in sortedFields) 
-                {
-                    if (bf.guiActive) label = "Toggle Off";
-                    else label = "Toggle On";
-                    GUILayout.Label(bf.name + "/" + bf.guiName);
-                    if (GUILayout.Button(label))
-                    {
-                        bf.guiActive = !bf.guiActive;
-                    }
-                }
-                GUILayout.EndScrollView();
-            }
-            if (GUILayout.Button("Show Events")) showEvents = !showEvents;
-            if (showEvents)
-            {
-                scrollPosition2 = GUILayout.BeginScrollView(scrollPosition2, GUILayout.Width(200), GUILayout.Height(200));
-                foreach (BaseEvent be in sortedEvents)
-                {
-                    if (be.guiActive) label = "Toggle Off";
-                    else label = "Toggle On";
-                    GUILayout.Label(be.guiName);
-                    if (GUILayout.Button(label))
-                    {
-                        be.guiActive = !be.guiActive;
-                    }
-                }
-                GUILayout.EndScrollView();
-            }
             if (GUILayout.Button("Show Vessels")) showVessels = !showVessels;
             if (showVessels)
             {
